1825661e5bb8ab9d882d59bb12a02b172f162b39
Adam Standish <devadamstandish@gmail.com>
2024-05-14 22:32:09 -0400

    initial commit

---
+++ backend/backend.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+  // globals
+  let ph = document.getElementById("intro");
+
+  ph.innerHTML = "This page will offer backend reference documentation.";
+});
